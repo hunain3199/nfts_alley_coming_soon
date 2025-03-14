365f481134618a8ff08d72dc397b70b555186a2a
hunain3199 <muhammadhunain3199@gmail.com>
2025-03-14 22:24:36 +0500

    hunain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Header from "@/components/otherComponents/Header";
+import Footer from "../components/otherComponents/Footer"
 export default function Home() {
   return (
     <div className="h-screen bg-gradient-to-r from-[#1E1629] via-[#FF00EA] to-[#1E1629] w-full">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/otherComponents/Footer.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const Footer = () => {
+  return (
+    <div className="h-screen bg-gradient-to-r from-[#842A7C] justify-between to-[#1E1629] w-full flex px-28 pt-28 text-white" >
+      <div className='flex flex-col'>
+        <div className='pb-10 '>
+            <Image src={'/logo.png'} alt='' height={70} width={100}/>
+        </div>
+        <div className='pb-5'>
+        BRD is the world's leading NFTs marketplace where <br />
+        you can discover, sell and bid NFTs and get rich.
+        </div>
+        <div className='flex gap-4 pb-5'>
+            <div><Image src={'/youtube.png'} alt='' height={30} width={30}/></div>
+            <div><Image src={'/twitter.png'} alt='' height={30} width={30}/></div>
+            <div><Image src={'/facebook.png'} alt='' height={30} width={30}/></div>
+            <div><Image src={'/google.png'} alt='' height={30} width={30}/></div>
+        </div>
+        <div className='pb-5'>
+        <span>All rights reserved<Link href="https://brdigitech.com" > Brdigitech</Link></span>
+
+        </div>
+      </div>
+
+      <div className='flex flex-col'>
+        <div className='font-bold text-3xl pb-10'>About</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>About NFT</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Live Auctions</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>NFT Blog</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Activity</div>
+      </div>
+
+      <div className='flex flex-col'>
+        <div className='font-bold text-3xl pb-10'>Support</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Help & Support</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Items Details</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Author Profile</div>
+        <div className='pb-5 text-xl text-[#9B9B9B]'>Collection</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
